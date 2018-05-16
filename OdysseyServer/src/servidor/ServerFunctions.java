346a092047a465a4ee77d69ccfcebe6acd2df6e7
Daniel Acuña M <daniel-2443@hotmail.com>
2018-05-16 05:03:01 -0600

    funciones del server

---
+++ OdysseyServer/src/servidor/ServerFunctions.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package servidor;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Base64;
+
+import org.farng.mp3.MP3File;
+import org.farng.mp3.TagException;
+import org.farng.mp3.id3.ID3v1_1;
+import org.farng.mp3.id3.ID3v2_2;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ * @author Daniel Acuña Mora
+ *
+ */
+public class ServerFunctions {
+
+	public static void setMeta() throws IOException, TagException {
+		MP3File mp3file = new MP3File("C:\\xml\\Juanpa.mp3");
+		ID3v2_2 tag = (ID3v2_2) mp3file.getID3v2Tag();
+		ID3v1_1 tag1 = (ID3v1_1) mp3file.getID3v1Tag();
+		tag.setSongTitle("8BIT");
+		tag.setAlbumTitle("Synth");
+		tag.setSongGenre("Retro");
+		tag1.setArtist("DAni");
+
+		System.out.println(tag.getAlbumTitle());
+		mp3file.save();
+		// File filemp3 = new File("C:\\xml\\Juanpa.mp3");
+		// File file2 = new File("C:\\xml\\"+tag.getSongTitle()+".mp3");
+		// filemp3.renameTo(file2);
+	}
+
+	public static void addSong(Document d, Socket s, String sd) throws IOException {
+		Document doc = d;
+		Socket clienteNuevo = s;
+		String lel = sd;
+		System.out.println("Agregamos una nueva cancion");
+		NodeList nodos = doc.getElementsByTagName("Song");
+		for (int ind = 0; ind < nodos.getLength(); ind++) {
+			Node nod = nodos.item(ind);
+			String encodedString = nod.getTextContent();
+			// System.out.println(encodedString);
+			byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
+			nodos = doc.getElementsByTagName("Nombre");
+			nod = nodos.item(0);
+			File file2 = new File("C:\\xml\\" + nod.getTextContent() + ".mp3");
+			FileOutputStream os = new FileOutputStream(file2, true);
+			os.write(decodedBytes);
+			os.close();
+			System.out.println("Respondiendo al cliente");
+			PrintStream resp = new PrintStream(clienteNuevo.getOutputStream());
+			resp.println(lel);
+			System.out.println("Mensaje enviado");
+			clienteNuevo.close();
+		}
+	}
+}
